7d45bbb03e3420190cb881155c00bf9262845992
lucasbdt <lucas.ssilva@bandtec.com.br>
2020-06-23 10:04:06 +0000

    up

--- APP4minds/app/src/main/java/br/com/bandtec/quatrominds/services/AuthInterceptor.kt
+++ APP4minds/app/src/main/java/br/com/bandtec/quatrominds/services/AuthInterceptor.kt
@@ -6,7 +6,7 @@ import okhttp3.Interceptor
 import okhttp3.Response
 import okhttp3.ResponseBody
 
-class AuthInterceptor (context: Context) : Interceptor 
+class AuthInterceptor (context: Context) : Interceptor {
     private val sessionManager = SessionManager(context)
 
     override fun intercept(chain: Interceptor.Chain): Response {
